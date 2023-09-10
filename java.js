29f11d7edbf148bc64497bb63d159e344d2604fe
Sam Jeffy <144205599+Jxffy@users.noreply.github.com>
2023-09-10 18:26:16 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const xo=document.querySelector('.xo');
+    const btnnav=document.querySelector('#btnnav');
+    const backdrop=document.querySelector('.backdrop');
+   });
+   
+   btnnav.addEventListener('click',()=>{
+       xo.classlist.add('open');
+   });
+
+   
